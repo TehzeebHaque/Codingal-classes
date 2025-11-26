5f0a6733f884b31fd6a09218a68fcbbddead6205
TehzeebHaque <tehzeb.haque.ariyan@gmail.com>
2025-11-26 21:51:04 +0600

    Shape class

---
+++ Pythoncodes/Trtl.py
@@ -0,0 +1,52 @@
+print("hi")
+
+#Activity-1
+import turtle
+# turtle.Screen().bgcolor("orange")
+# turtle.Screen().setup(300,400)
+# polygon = turtle.Turtle()
+# num_sides = 6
+# side_length = 70
+# angle = 360.0 / num_sides
+# for i in range (num_sides) :
+#     polygon.forward(side_length)
+#     polygon.right(angle)
+# turtle.done()
+
+#Activity-2
+turtle.Screen().bgcolor("yellow")
+board = turtle.Turtle()
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+
+turtle.done()
+
+#Activity-3
+my_wn = turtle.Screen()
+my_wn.bgcolor("lightblue")
+my_wn.title("Hello, Tess!")
+my_pen = turtle.Turtle()
+size=0
+while True: 
+    for i in range(4):
+      my_pen.fd(size + 1)
+      my_pen.left(90)
+      size = size-5
+    size = size+1
